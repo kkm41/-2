800161028445de95aa3020e64461c88a5e9c0747
DESKTOP-F4BC13L\hi <ssbf41@gmail.com>
2024-03-20 17:15:28 +0900

    변수

--- 01-lecture-source/chap01-literal-and-variable-lecture-source/src/variable/Application1.java
+++ 01-lecture-source/chap01-literal-and-variable-lecture-source/src/variable/Application1.java
@@ -46,6 +46,20 @@ public class Application1 {
         System.out.println("9번 고객에게 포인트를 " + point + "포인트 지급하였습니다");
         System.out.println("10번 고객에게 포인트를 " + point + "포인트 지급하였습니다");
 
+        /*3. 시간에 따라 변경되는 값을 정하고 사용할 수 있다*/
+        System.out.println("=====================변수에 저장된 값 변경 테스트============================");
+
+        int sum = 0;
+
+        sum = sum + 10;
+        System.out.println("sum에 10을 더하면 현재의 sum값은? " +  sum);
+
+        sum = sum + 10;
+        System.out.println("sum에 10이 있었으니 10을 추가로 더하면 sum의 값은? "  +  sum);
+
+        sum = sum + 10;
+        System.out.println("sum에 20이 있었으니 10을 추가로 더하면 sum의 값은? "  +  sum);
+
 
 
 
